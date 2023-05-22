5a548d32fa023cdb4369613611d7d13374014feb
Gabriel Mors <gabrielmors210@gmail.com>
2023-05-22 06:36:34 -0400

    adicionando mais foto

--- Projeto-Instagram/Home/ViewModel/HomeViewModel.swift
+++ Projeto-Instagram/Home/ViewModel/HomeViewModel.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class HomeViewModel {
 
-    private var posts = [Posts(profileImage: "perfil.fer", userName: "Fernando", postImage: "post.fer"),
+    private var posts = [Posts(profileImage: "perfil.vitor", userName: "Mini Chan", postImage: "post-vitor"),
                         Posts(profileImage: "perfil.fer", userName: "Fernando", postImage: "post.fer"),
+                        Posts(profileImage: "perfil.chico", userName: "Chico", postImage: "post-chico"),
+                        Posts(profileImage: "perfil.marada", userName: "Marada", postImage: "post-marada"),
+                        Posts(profileImage: "perfil.diego", userName: "Diego", postImage: "post-diego"),
+                        Posts(profileImage: "perfil.enzo", userName: "Enzo", postImage: "post-enzo"),
+                         Posts(profileImage: "perfil.samuca", userName: "Samuel", postImage: "post-samuca"),
+                        Posts(profileImage: "perfil.dasa", userName: "Dasaev", postImage: "post-dasa"),
                         Posts(profileImage: "perfil.fer", userName: "Fernando", postImage: "post.fer"),
                         Posts(profileImage: "perfil.fer", userName: "Fernando", postImage: "post.fer"),
                         Posts(profileImage: "perfil.fer", userName: "Fernando", postImage: "post.fer"),
@@ -25,7 +31,13 @@ class HomeViewModel {
     }
     
     private var story = [Stories(Image: "foto.perfil", userName: "Add Story"),
+                         Stories(Image: "perfil.enzo", userName: "Enzo"),
+                         Stories(Image: "perfil.chico", userName: "Chico"),
+                         Stories(Image: "perfil.marada", userName: "Maradona"),
                          Stories(Image: "perfil.fer", userName: "Fernando"),
+                         Stories(Image: "perfil.samuca", userName: "Samuel"),
+                         Stories(Image: "perfil.dasa", userName: "Dasaev"),
+                         Stories(Image: "perfil.arthur", userName: "Arthurito"),
                          Stories(Image: "foto03", userName: "Marcelo"),
                          Stories(Image: "foto04", userName: "Tiago"),
                          Stories(Image: "foto05", userName: "Vitor"),
